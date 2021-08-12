691dc44fac3df9b891ec428ec3000dbea2e79065
Circus-Pygargus <richard.meuret@laposte.fr>
2021-08-12 15:01:37 +0000

    [Recette] création RecipeController et ajout gestion de la route de création de recette et de son formulaire

---
+++ src/Controller/Admin/RecipeController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Controller\Admin\AdminController;
+use App\Entity\Recipe;
+use App\Form\Recipe\AddRecipeFormType;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class RecipeController
+ * @package App\Controller\Admin
+ * @Route("/admin/recipes", name="admin_recipes_")
+ */
+class RecipeController extends AdminController
+{
+    /**
+     * @Route("/", name="list")
+     */
+    public function list (): Response
+    {
+        return new Response();
+    }
+
+    /**
+     * @Route("/create", name="create")
+     */
+    public function create (Request $request): Response
+    {
+        $this->navigationInfos[] = [
+            'text' => 'Gérer les recettes',
+            'urlPath' => 'admin_recipes_list'
+        ];
+        $this->navigationInfos[] = [
+            'text' => 'Créer une recette',
+            'urlPath' => 'admin_recipes_create'
+        ];
+        $this->contentNavigation['inUseRegex'] = 'admin_recipes';
+
+        $recipe = new Recipe();
+
+        $form = $this->createForm(AddRecipeFormType::class, $recipe);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($recipe);
+            $entityManager->flush();
+        }
+
+        return $this->render('admin/recipe/create.html.twig', [
+            'heroImgName' => $this->heroImgName,
+            'navigationInfos' => $this->navigationInfos,
+            'contentNavigation' => $this->contentNavigation,
+            'recipeForm' => $form->createView()
+        ]);
+    }
+}
