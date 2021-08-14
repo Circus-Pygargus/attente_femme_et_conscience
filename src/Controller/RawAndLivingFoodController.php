17f38d3f4d3ff043661ba571bda3bbc8f2532d5b
Circus-Pygargus <richard.meuret@laposte.fr>
2021-08-14 23:38:38 +0000

    [Recette] recipeController (user side) : création route pour afficher les recettes d'une catégorie + réécriture des routes + les variables communes en var privées

--- src/Controller/RawAndLivingFoodController.php
+++ src/Controller/RawAndLivingFoodController.php
@@ -2,26 +2,39 @@
 
 namespace App\Controller;
 
+use App\Entity\Recipe;
+use App\Entity\RecipeCategory;
 use App\Repository\Traits\RecipeTrait;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class ArticleController
+ * @package App\Controller\Admin
+ * @Route("/alimentation-crue-et-vivante", name="raw_and_living_food")
+ */
 class RawAndLivingFoodController extends AbstractController
 {
     use RecipeTrait;
 
-    /**
-     * @Route("/alimentation-crue-et-vivante", name="raw_and_living_food")
-     */
-    public function index (EntityManagerInterface $entityManager): Response
+    private array $navbarInfos;
+    private array $navigationInfos;
+    private array $contentNavigation;
+    private array $content;
+    private string $heroImgName;
+
+    public function __construct(EntityManagerInterface $entityManager)
     {
-        $navbarInfos = [
+        $this->heroImgName = 'recette-hero.jpg';
+        $this->navbarInfos = [
             'page' => 'raw-and-living-food'
         ];
-        $navigationInfos = [
+        $this->navigationInfos = [
             [
                 'text' => 'Accueil',
                 'urlPath' => 'home'
@@ -31,16 +44,77 @@ class RawAndLivingFoodController extends AbstractController
                 'urlPath' => 'raw_and_living_food'
             ]
         ];
-        $contentNavigation = $this->getRecipesAndRelatedLinks($entityManager);
-        $contentNavigation['inUseRegex'] = 'raw_and_living_food';
-        $content['page'] = 'Présentation';
+        $this->contentNavigation = [];
+        $this->content = [];
+        $this->em = $entityManager;
+    }
+
+    /**
+     * @Route("/", name="")
+     */
+    public function index (): Response
+    {
+        $this->contentNavigation = $this->getRecipesAndRelatedLinks($this->em);
+        $this->contentNavigation['inUseRegex'] = 'raw_and_living_food';
+        $this->content['page'] = 'Présentation';
 
 
         return $this->render('raw-and-living-food/index.html.twig', [
-            'navbarInfos' => $navbarInfos,
-            'navigationInfos' => $navigationInfos,
-            'contentNavigation' => $contentNavigation,
-            'content' => $content
+            'navbarInfos' => $this->navbarInfos,
+            'navigationInfos' => $this->navigationInfos,
+            'contentNavigation' => $this->contentNavigation,
+            'content' => $this->content
+        ]);
+    }
+
+    /**
+     * @Route("/recettes-{slug}s", name="_category")
+     */
+    public function category (
+        string $slug,
+        Request $request,
+        PaginatorInterface $paginator
+    ): Response
+    {
+        $recipeCategory = $this->em->getRepository(RecipeCategory::class)->findOneBy(['slug' => $slug]);
+        $this->navigationInfos[] = [
+            'text' => 'Recettes ' . $recipeCategory->getLabel() . 's',
+            'urlPath' => 'raw_and_living_food_category',
+            'slug' => $slug
+        ];
+        $this->contentNavigation = $this->getRecipesAndRelatedLinks($this->em);
+        $this->contentNavigation['inUseRegex'] = 'raw_and_living_food';
+        $this->content = [
+            'page' => 'Recettes ' . $recipeCategory->getLabel() . 's',
+            'noContentMsg' => 'Pas de recettes ' . $recipeCategory->getLabel() . 's pour le moment.',
+            'subLayout' => 'recipe.html.twig'
+        ];
+
+        $recipesData = $this->em
+            ->getRepository(Recipe::class)
+            ->findBy([
+                'category' => $recipeCategory->getId(),
+                'published' => true
+            ],
+            [
+                'createdAt' => 'DESC'
+            ],
+            10, // limit
+            0 // offset
+       );
+
+        $this->content['list'] = $paginator->paginate(
+            $recipesData, // Requête contenant les données à paginer
+            $request->query->getInt('page', 1), // Numéro de la page demandée via url, 1 si aucune page
+            2 // le nombre de recettes par page
+        );
+
+        return $this->render('blog-content/index.html.twig', [
+            'heroImgName' => $this->heroImgName,
+            'navbarInfos' => $this->navbarInfos,
+            'navigationInfos' => $this->navigationInfos,
+            'contentNavigation' => $this->contentNavigation,
+            'content' => $this->content
         ]);
     }
 }
