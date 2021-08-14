93f4b21f32ccf98ef6ac3053b97ed36eac6c2ade
Circus-Pygargus <richard.meuret@laposte.fr>
2021-08-14 17:12:19 +0000

    [Recette] Création de RecipteTrait pour récupérer les liens de recettes pour la colonne de navigation (coté utilisateur)

---
+++ src/Repository/Traits/RecipeTrait.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Repository\Traits;
+
+use App\Entity\Recipe;
+use App\Entity\RecipeCategory;
+use App\Repository\RecipeRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+
+Trait RecipeTrait
+{
+    protected function getRecipesAndRelatedLinks (EntityManagerInterface $entityManager): array
+    {
+        $title = 'Alimentation';
+        $categories = [];
+        $categories[] = [
+            'name' => 'Présentation',
+            'link' => 'raw_and_living_food'
+        ];
+
+        $emRecipeCategory = $entityManager->getRepository(RecipeCategory::class);
+        $recipeCategories = $emRecipeCategory->findAll();
+
+        $emRecipe = $entityManager->getRepository(Recipe::class);
+        foreach ($recipeCategories as $recipeCategory) {
+            $sweetyRecipes = $emRecipe->findBy([
+                'category' => $recipeCategory->getId(),
+                'published' => true
+                ],
+                [
+                    'createdAt' => 'DESC'
+                ],
+                10, // limit
+                0 // offset
+            );
+            $categories[] = [
+                'name' => 'Recettes ' . $recipeCategory->getLabel() . 's',
+                'link' => 'raw_and_living_food',
+                'links' => $sweetyRecipes
+            ];
+        }
+
+        return [
+            'title' => $title,
+            'categories' => $categories
+        ];
+    }
+}
