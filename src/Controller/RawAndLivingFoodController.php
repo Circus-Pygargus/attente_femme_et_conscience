3f15eaa061337c8e776e985e322ccb8b954818fb
Circus-Pygargus <richard.meuret@laposte.fr>
2021-08-14 17:14:16 +0000

    [Recette] RecipeController (user side) : récupération des liens liés aux recettes et envoi vers la vue

--- src/Controller/RawAndLivingFoodController.php
+++ src/Controller/RawAndLivingFoodController.php
@@ -2,16 +2,21 @@
 
 namespace App\Controller;
 
+use App\Repository\Traits\RecipeTrait;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RawAndLivingFoodController extends AbstractController
 {
+    use RecipeTrait;
+
     /**
      * @Route("/alimentation-crue-et-vivante", name="raw_and_living_food")
      */
-    public function index (): Response
+    public function index (EntityManagerInterface $entityManager): Response
     {
         $navbarInfos = [
             'page' => 'raw-and-living-food'
@@ -26,9 +31,16 @@ class RawAndLivingFoodController extends AbstractController
                 'urlPath' => 'raw_and_living_food'
             ]
         ];
+        $contentNavigation = $this->getRecipesAndRelatedLinks($entityManager);
+        $contentNavigation['inUseRegex'] = 'raw_and_living_food';
+        $content['page'] = 'Présentation';
+
+
         return $this->render('raw-and-living-food/index.html.twig', [
             'navbarInfos' => $navbarInfos,
-            'navigationInfos' => $navigationInfos
+            'navigationInfos' => $navigationInfos,
+            'contentNavigation' => $contentNavigation,
+            'content' => $content
         ]);
     }
 }
