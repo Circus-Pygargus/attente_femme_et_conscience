9e269c1d7d4afdd20276884bfe6ea571a5a72f7a
Circus-Pygargus <richard.meuret@laposte.fr>
2021-08-22 11:37:46 +0000

    [FoodArticle] controller pour admin : création de la méthode edit()

--- src/Controller/Admin/FoodArticleController.php
+++ src/Controller/Admin/FoodArticleController.php
@@ -3,7 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Controller\Admin\AdminController;
+use App\Entity\Article;
 use App\Entity\FoodArticle;
+use App\Form\Article\EditFoodArticleFormType;
 use App\Form\FoodArticle\AddFoodArticleFormType;
 use App\Form\FoodArticle\PublishFoodArticleFormType;
 use App\Repository\FoodArticleRepository;
@@ -108,6 +110,51 @@ class FoodArticleController extends AdminController
         }
 
         return $this->render('admin/food-article/create.html.twig', [
+            'heroImgName' => $this->heroImgName,
+            'navigationInfos' => $this->navigationInfos,
+            'contentNavigation' => $this->contentNavigation,
+            'foodArticleForm' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("edite/{slug}", name="edit")
+     */
+    public function edit (
+        string $slug = '',
+        Request $request,
+        FoodArticleRepository $foodArticleRepository
+    ): Response
+    {
+        $this->navigationInfos[] = [
+            'text' => 'Gérer les articles liés à  la nourriture',
+            'urlPath' => 'admin_food_articles_list'
+        ];
+        $this->navigationInfos[] = [
+            'text' => 'Éditer un article lié à la nourriture',
+            'urlPath' => 'admin_food_articles_edit'
+        ];
+        $this->contentNavigation['inUseRegex'] = 'admin_articles';
+
+        if ($slug !== '') {
+            $foodArticle = $foodArticleRepository->findOneBy(['slug' => $slug]);
+        } else {
+            // TODO modifier pour un message d'erreur ??? et le faire pour toutes les entités !!!
+            $foodArticle = new Article();
+        }
+
+        $form = $this->createForm(EditFoodArticleFormType::class, $foodArticle);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $doctrine = $this->getDoctrine()->getManager();
+            $doctrine->persist($foodArticle);
+            $doctrine->flush();
+            return $this->redirectToRoute('admin_food_articles_list');
+        }
+
+        return $this->render('admin/food-article/edit.html.twig', [
             'heroImgName' => $this->heroImgName,
             'navigationInfos' => $this->navigationInfos,
             'contentNavigation' => $this->contentNavigation,
